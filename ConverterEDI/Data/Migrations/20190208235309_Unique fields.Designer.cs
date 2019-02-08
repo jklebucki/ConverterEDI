7bf9ac0a9ebdb94540707ea7c275c84608f25193
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2019-02-09 00:55:05 +0100

    Nowe migracje dla indeksów

--- ConverterEDI/Data/Migrations/20190208235309_Unique fields.Designer.cs
+++ ConverterEDI/Data/Migrations/20190208235309_Unique fields.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using ConverterEDI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ConverterEDI.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190208235309_Unique fields")]
+    partial class Uniquefields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
